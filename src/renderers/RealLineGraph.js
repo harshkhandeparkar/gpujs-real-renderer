aead8491e4bd9a5586837ff98dd3b1baa0248ce9
Mr. X <mjkgoa@gmail.com>
2020-04-07 18:35:13 +0530

    Change RealLineGraph

--- src/renderers/RealLineGraph.js
+++ src/renderers/RealLineGraph.js
@@ -1,38 +1,26 @@
 const RealRenderer = require('./RealRenderer');
-const getDrawKernel = require('../kernels/draw');
+const getProgressGraphKernel = require('../kernels/progressGraph');
 
 class RealLineGraph extends RealRenderer {
   constructor(options) {
     super(options);
 
-    this.progressiveAxis = options.progressiveAxis.toLowerCase() || 'x'; // Which axis remains static
+    this.progressiveAxis = options.progressiveAxis || 'x'; // Which axis progresses with time
+    this.progressiveAxis = this.progressiveAxis.toLowerCase();
     this.progressionMode = options.progressionMode || 'overflow'; // overflow -> Only progresses when completely filled; continous -> Always progresses;
-    this.progressionRatio = options.progressionRatio || 1; // One time unit = progressionRatio coordinate units
+    this.progressionRatio = options.progressInterval || 1; // Progress once every interval time units; Only works with continous progressionMode
     this.brushSize = options.brushSize;
 
-    this.graphInputFunc = gpu.createKernel(options.graphInputFunc, {output: this.dimensions.x, pipeline: true});
+    this._progressGraph = getProgressGraphKernel(this.gpu, this.dimensions, this.progressiveAxis);
 
-    const drawKernel = getDrawKernel(
-      this.gpu,
-      this.dimensions,
-      this.xScaleFactor,
-      this.yScaleFactor,
-      function(x, y, pix, time, data) {
-        x += (time*progressionRatio) / xScaleFactor;
+    this.lastProgress = 0; // Time when the graph last progressed. Internal variable
+  }
 
-        if (Math.abs(y - data[this.thread.x + Math.floor(time*this.constants.progressionRatio)]) <= this.constants.brushSize) return [1, 0, 0];
-      },
-      {
-        brushSize: this.brushSize,
-        progressionRatio: this.progressionRatio
-      }
-    )
-
-    this.drawFunc = (graphPixels, time) => {
-      this.data = this.graphInputFunc(time);
-
-      return drawKernel(graphPixels, time, data);
+  _drawFunc(graphPixels, time) {
+    if (time - this.lastProgress >= this.progressInterval) {
+      return this._progressGraph(graphPixels, time);
     }
+    else return graphPixels;
   }
 }
 
