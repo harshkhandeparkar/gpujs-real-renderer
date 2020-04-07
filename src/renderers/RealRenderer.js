8fa2dcb3fd4c0d2e870f3372d0c5a78cfa809be2
Mr. X <mjkgoa@gmail.com>
2020-04-07 19:06:20 +0530

    changes in RealRenderer

--- src/renderers/RealRenderer.js
+++ src/renderers/RealRenderer.js
@@ -1,5 +1,6 @@
-const GPU = window.GPU;
 const getDisplayKernel = require('../kernels/display');
+const getBlankGraphKernel = require('../kernels/blankGraph');
+const getCloneTextureKernel = require('../kernels/cloneTexture')
 
 class RealRenderer {
   constructor(options) {
@@ -12,8 +13,16 @@ class RealRenderer {
     this.drawsPerFrame = options.drawsPerFrame || 1;
     this.timeStep = options.timeStep || (1 / 60);
 
-    this.xOffset = options.xOffset || 50; // %age offset
-    this.yOffset = options.yOffset || 50; // %age offset
+    this.xOffset = options.xOffset; // %age offset
+    this.yOffset = options.yOffset; // %age offset
+
+    options.GPU = options.GPU || window.GPU;
+
+    if (typeof this.xOffset != 'number') this.xOffset = 50;
+    if (typeof this.yOffset != 'number') this.yOffset = 50;
+
+    this.xOffset = Math.max(0, Math.min(100, this.xOffset)) // Between 0 and 100
+    this.yOffset = Math.max(0, Math.min(100, this.yOffset)) // Between 0 and 100
 
     if (document.getElementById(this.canvasTag) === undefined) {
       throw 'No Canvas Element Found';
@@ -21,56 +30,80 @@ class RealRenderer {
 
     this.canvas = document.getElementById(this.canvasTag);
 
-    this.gpu = new GPU({
+    this.gpu = new options.GPU({
       canvas: this.canvas,
       mode: 'gpu'
     })
-    this.graphPixels = blankGraph();
 
-    this.display = getDisplayKernel(this.gpu, this.dimensions);
+    this._blankGraph = getBlankGraphKernel(this.gpu, this.dimensions, this.xOffset, this.yOffset, this.bgColor, this.axesColor);
+    this._cloneTexture = getCloneTextureKernel(this.gpu, this.dimensions);
+
+    this.graphPixels = this._blankGraph();
+
+    this._display = getDisplayKernel(this.gpu, this.dimensions);
 
     this.time = options.time || 0;
+    this.doRender = false;
+  }
 
-    this.doRender = true;
+  _drawFunc(graphPixels /*, time*/) { // Can be overridden
+    return graphPixels;
   }
 
   _draw() {
     this.time += this.timeStep;
-    this.graphPixels = this.drawFunc(this.graphPixels, this.time); // drawFunc defined by child class
+
+    this.graphPixels = this._drawFunc(this.graphPixels, this.time);
   }
 
-  draw(numDraws) {
+  draw(numDraws = 1) {
     for (let i = 0; i < numDraws; i++) this._draw();
-    this.display(this.graphPixels);
+    this._display(this.graphPixels);
+
+    return this;
   }
 
   _render() {
     for (let i = 0; i < this.drawsPerFrame; i++) this._draw();
-    this.display(this.graphPixels);
+    this._display(this.graphPixels);
 
-    if (this.doRender) window.requestAnimationFrame(this.startRender);
+    if (this.doRender) window.requestAnimationFrame(() => {this._render()});
   }
 
   startRender() {
     this.doRender = true;
-    this._render;
+    this._render();
+
+    return this;
   }
 
   stopRender() {
     this.doRender = false;
+
+    return this;
   }
 
   resetTime() {
-    this.stopRender();
     this.time = 0;
-    this.startRender();
+
+    return this;
   }
 
   clearPixels() {
-    this.stopRender();
+    let initialRender = false;
+
+    if (this.doRender) {
+      initialRender = true;
+      this.stopRender();
+    }
+
     this.graphPixels.delete();
-    this.graphPixels = blankGraph();
-    this.startRender();
+    this.graphPixels = this._blankGraph();
+    this._display(this.graphPixels);
+
+    if (initialRender) this.startRender();
+
+    return this;
   }
 }
 
