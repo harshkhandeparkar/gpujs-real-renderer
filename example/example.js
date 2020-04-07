a8a029cef976af4b5730dc196d1af705ce8ace75
Mr. X <mjkgoa@gmail.com>
2020-04-07 20:31:39 +0530

    change options in example JS

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
 const LineGraph = new GPUjsRealRenderer.RealLineGraph({
   canvasTag: 'line-canvas',
   dimensions: [1280, 720],
-  xOffset: 30, // 20%
+  xOffset: 20, // 20%
   yOffset: 10,  // 80%
   xScaleFactor: 20,
   yScaleFactor: 10,
