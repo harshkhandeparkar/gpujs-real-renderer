c59c27fc373f5ce519b9fb1a2dbc4440d113f7a5
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-23 15:19:14 +0530

    fix: convert cloneTexture kernel

---
+++ src/kernels/cloneTexture.ts
@@ -0,0 +1,19 @@
+import { GPU } from 'gpu.js';
+import { GraphDimensions } from '../renderers/RealRenderer';
+
+/**
+ * @param gpu GPU.js instance
+ * @param dimensions Dimensions of Graph
+ */
+export function getCloneTextureKernel(
+  gpu: GPU,
+  dimensions: GraphDimensions
+) {
+  return gpu.createKernel(function(graphPixels) {
+    return graphPixels[this.thread.y][this.thread.x];
+  },
+  {
+    output: dimensions,
+    pipeline: true
+  })
+}
