c1293e912aede2006ac06b1dd7203f69d2ec2aa5
HarshKhandeparkar <mjkgoa@gmail.com>
2020-08-23 13:03:43 +0530

    feat: install typescript

--- index.js
+++
@@ -1,9 +0,0 @@
-const RealRenderer = require('./src/renderers/RealRenderer');
-const RealLineGraph = require('./src/renderers/RealLineGraph');
-const RealComplexSpace = require('./src/renderers/RealComplexSpace');
-
-module.exports = {
-  RealRenderer,
-  RealLineGraph,
-  RealComplexSpace
-}
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { RealRenderer } from './src/renderers/RealRenderer';
+export { RealRenderer } from './src/renderers/RealRenderer';
+export { RealLineGraph } from './src/renderers/RealLineGraph';
+export { RealComplexSpace } from './src/renderers/RealComplexSpace';
+
+export default {
+  RealRenderer
+}
