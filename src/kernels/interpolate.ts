ace8c20dff29130b823ebdbf2bb5da5037894dc5
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-22 10:38:03 +0530

    fix: double line thickness and vertical interpolation

--- src/kernels/interpolate.ts
+++ src/kernels/interpolate.ts
@@ -1,9 +1,20 @@
-import { GPU } from 'gpu.js';
+import { GPU, IKernelFunctionThis } from 'gpu.js';
 import { GraphDimensions, Color } from '../types/RealRendererTypes';
 
+export interface IInterpolateKernelThis extends IKernelFunctionThis {
+  constants: {
+    lineThickness: number,
+    lineColor: [number, number, number],
+    xScaleFactor: number,
+    yScaleFactor: number,
+    xOffset: number,
+    yOffset: number
+  }
+}
+
 /**
- * @param gpu 
- * @param dimensions 
+ * @param gpu
+ * @param dimensions
  * @param xScaleFactor
  * @param yScaleFactor
  * @param xOffset
@@ -22,30 +33,37 @@ export function getInterpolateKernel(
   lineColor: Color
 ) {
   return gpu.createKernel(
-    function(graphPixels: any, val1: [number, number], val2: [number, number]) {
+    function(
+      this: IInterpolateKernelThis,
+      graphPixels: any,
+      val1: [number, number],
+      val2: [number, number]
+    ) {
       const x = this.thread.x,
         y = this.thread.y;
+
+      const lineHalfThickness = this.constants.lineThickness / 2;
 
       const x1 = val1[0];
       const y1 = val1[1];
 
       const x2 = val2[0];
       const y2 = val2[1];
-        
+
       const outX = this.output.x, outY = this.output.y;
 
-      const X = x / (this.constants.xScaleFactor as number) - (outX * (this.constants.yOffset as number / 100)) / (this.constants.xScaleFactor as number);
-      const Y = y / (this.constants.yScaleFactor as number) - (outY * (this.constants.xOffset as number / 100)) / (this.constants.yScaleFactor as number);
+      const X = x / (this.constants.xScaleFactor) - (outX * (this.constants.yOffset / 100)) / (this.constants.xScaleFactor);
+      const Y = y / (this.constants.yScaleFactor) - (outY * (this.constants.xOffset / 100)) / (this.constants.yScaleFactor);
 
       let lineEqn = X * (y1 - y2) - x1 * (y1 - y2) - Y * (x1 - x2) + y1 * (x1 -x2);
       let lineDist = Math.abs(lineEqn) / Math.sqrt((y1 - y2)*(y1 - y2) + (x1 - x2)*(x1 - x2));
 
       if (
-        lineDist <= this.constants.lineThickness &&
-        X <= Math.max(x1, x2) &&
-        X >= Math.min(x1, x2) &&
-        Y <= Math.max(y1, y2) &&
-        Y >= Math.min(y1, y2)
+        lineDist <= lineHalfThickness &&
+        X <= Math.max(x1, x2) + lineHalfThickness &&
+        X >= Math.min(x1, x2) - lineHalfThickness &&
+        Y <= Math.max(y1, y2) + lineHalfThickness &&
+        Y >= Math.min(y1, y2) - lineHalfThickness
       ) return this.constants.lineColor;
       else return graphPixels[this.thread.y][this.thread.x];
     },
