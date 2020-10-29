39ec739203608309dac506ce46a288d1868dfc67
HarshKhandeparkar <mjkgoa@gmail.com>
2020-10-29 10:57:06 +0530

    feat<RealDrawBoard>: method to calculate touch coords

--- src/renderers/RealDrawBoard/_coords.ts
+++ src/renderers/RealDrawBoard/_coords.ts
@@ -12,3 +12,24 @@ export function _getMouseCoords(
 
   return [x, y]; // In graph coordinates
 }
+
+export function _getTouchCoords(
+  this: RealDrawBoard,
+  e: TouchEvent
+): [number, number][] {
+  let allCoords: [number, number][] = [];
+
+  for (let i = 0; i < e.touches.length; i++) {
+    const touch = e.touches.item(i);
+
+    let x = touch.clientX - this.canvas.getBoundingClientRect().left;
+    let y = this.dimensions[1] - (touch.clientY - this.canvas.getBoundingClientRect().top);
+
+    x = x / this.xScaleFactor - (this.dimensions[0] * (this.yOffset / 100)) / this.xScaleFactor;
+    y = y / this.yScaleFactor - (this.dimensions[1] * (this.xOffset / 100)) / this.yScaleFactor;
+
+    allCoords.push([x, y]);
+  }
+
+  return allCoords;
+}
