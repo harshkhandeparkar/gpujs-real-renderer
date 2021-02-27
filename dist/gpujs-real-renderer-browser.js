cb5c27e2007564fa698ed1ecfefddf05c70f42ff
HarshKhandeparkar <mjkgoa@gmail.com>
2021-02-27 09:29:53 +0530

    v0.7.0

--- dist/gpujs-real-renderer-browser.js
+++ dist/gpujs-real-renderer-browser.js
@@ -734,7 +734,7 @@
 	/**
 	 * Convert Cartesian to polar form.
 	 * @param x Real Part
-	 * @param theta Complex Part
+	 * @param y Complex Part
 	 */
 	function convertCartesianPolar(x, y) {
 	    return [
@@ -1144,14 +1144,105 @@
 	exports._toolPreview = _toolPreview;
 	});
 
+	var convertHSLToRGB_1 = createCommonjsModule(function (module, exports) {
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.convertHSLToRGB = void 0;
+	/**
+	 * Convert hsl Color into RGB color.
+	 * @param h color value ranges from 0 to 360.
+	 * @param s saturation value.
+	 * @param l brightness level.
+	 * @return array containing r g and b value
+	 */
+	function convertHSLToRGB(h, s, l) {
+	    s /= 100;
+	    l /= 100;
+	    var c = (1 - Math.abs(2 * l - 1)) * s;
+	    var x = c * (1 - Math.abs((h / 60) % 2 - 1));
+	    var m = l - c / 2;
+	    var r = 0;
+	    var g = 0;
+	    var b = 0;
+	    if (0 <= h && h < 60) {
+	        r = c;
+	        g = x;
+	        b = 0;
+	    }
+	    else if (60 <= h && h < 120) {
+	        r = x;
+	        g = c;
+	        b = 0;
+	    }
+	    else if (120 <= h && h < 180) {
+	        r = 0;
+	        g = c;
+	        b = x;
+	    }
+	    else if (180 <= h && h < 240) {
+	        r = 0;
+	        g = x;
+	        b = c;
+	    }
+	    else if (240 <= h && h < 300) {
+	        r = x;
+	        g = 0;
+	        b = c;
+	    }
+	    else if (300 <= h && h < 360) {
+	        r = c;
+	        g = 0;
+	        b = x;
+	    }
+	    r = (r + m);
+	    g = (g + m);
+	    b = (b + m);
+	    return [r, g, b];
+	}
+	exports.convertHSLToRGB = convertHSLToRGB;
+	});
+
+	var rainbow_brush = createCommonjsModule(function (module, exports) {
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports._toolPreview = exports._doStroke = exports._endStroke = exports._startStroke = exports.name = void 0;
+
+	var hue = 0;
+	var gradientColors = [1, 1, 1];
+	exports.name = 'rainbow_brush';
+	function _startStroke(coords, identifier) {
+	    gradientColors = convertHSLToRGB_1.convertHSLToRGB(hue, 90, 40);
+	    this._doPreview = false;
+	    this._plot(coords[0], coords[1], this.brushSize, gradientColors);
+	}
+	exports._startStroke = _startStroke;
+	function _endStroke(endCoords, identifier) {
+	    gradientColors = convertHSLToRGB_1.convertHSLToRGB(hue, 90, 40);
+	    this._plot(endCoords[0], endCoords[1], this.brushSize, gradientColors);
+	    this._doPreview = true;
+	}
+	exports._endStroke = _endStroke;
+	function _doStroke(coords, identifier) {
+	    hue = (hue + 1) % 360;
+	    gradientColors = convertHSLToRGB_1.convertHSLToRGB(hue, 90, 40);
+	    this._plot(coords[0], coords[1], this.brushSize, gradientColors);
+	    this._stroke(coords[0], coords[1], this.brushSize, gradientColors, identifier);
+	}
+	exports._doStroke = _doStroke;
+	function _toolPreview(coords, identifier) {
+	    return this._previewPlot(this.graphPixels, coords[0], coords[1], this.brushSize, gradientColors);
+	}
+	exports._toolPreview = _toolPreview;
+	});
+
 	var tools = createCommonjsModule(function (module, exports) {
 	Object.defineProperty(exports, "__esModule", { value: true });
 	exports.tools = void 0;
 
 
 
+
 	exports.tools = {
 	    brush: brush,
+	    rainbow_brush: rainbow_brush,
 	    eraser: eraser,
 	    line: line
 	};
